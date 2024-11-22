4f930e3bc7047dd8f60039294e459f6e7b9e5760
Francisco Villamil <franvillamil@gmail.com>
2024-11-22 09:24:16 +0100

    autoupdate

--- update.R
+++ update.R
@@ -11,12 +11,21 @@ urls_funds = c(
   "https://www.finect.com/fondos-inversion/IE0007472990-Vanguard__govt_bd_idx__acc",
   "https://www.finect.com/fondos-inversion/IE00B18GC888-Vanguard_global_bd_idx_eur_h_acc")
 
+
+
+# urls_stocks = data.frame(
+#   Stock = c("NOVA US", "IBE", "VOW GY"),
+#   u = c(
+#   "https://tools.morningstar.es/es/stockreport/default.aspx?SecurityToken=0P0001I1Y5]3]0]E0WWE$$ALL",
+#   "https://tools.morningstar.es/es/stockreport/default.aspx?SecurityToken=0P0000A4Z3%5D3%5D0%5DE0WWE%24%24ALL",
+#   "https://tools.morningstar.es/es/stockreport/default.aspx?SecurityToken=0P0000EP8W%5D3%5D0%5DE0WWE%24%24ALL"))
+
 urls_stocks = data.frame(
   Stock = c("NOVA US", "IBE", "VOW GY"),
   u = c(
-  "https://tools.morningstar.es/es/stockreport/default.aspx?SecurityToken=0P0001I1Y5]3]0]E0WWE$$ALL",
-  "https://tools.morningstar.es/es/stockreport/default.aspx?SecurityToken=0P0000A4Z3%5D3%5D0%5DE0WWE%24%24ALL",
-  "https://tools.morningstar.es/es/stockreport/default.aspx?SecurityToken=0P0000EP8W%5D3%5D0%5DE0WWE%24%24ALL"))
+    "https://www.morningstar.com/stocks/xnys/nova/quote",
+    "https://www.morningstar.com/stocks/xmad/ibe/quote",
+    "https://www.morningstar.com/stocks/xfra/vow/quote"))
 
 today = as.character(format(Sys.time(), "%Y-%m-%d"))
 
@@ -61,24 +70,24 @@ if(!today %in% olddf_funds$fecha){
 
 if(!today %in% olddf_stocks$fecha){
 
-  print(paste("[STOCKS] Updating with data from today:", today))
+  # print(paste("[STOCKS] Updating with data from today:", today))
 
-  df_stocks = data.frame(Stock = NA, precio = NA, fecha = NA)
+  # df_stocks = data.frame(Stock = NA, precio = NA, fecha = NA)
 
-  for(i in 1:nrow(urls_stocks)){
+  # for(i in 1:nrow(urls_stocks)){
 
-    precio = urls_stocks$u[i] %>%
-      read_html() %>% 
-      html_nodes(xpath = '//*[@id="Col0Price"]') %>%
-      html_text()
-    precio = as.numeric(gsub(",", ".", precio))
-    dfr = data.frame(Stock = urls_stocks$Stock[i], precio = precio, fecha = today)
-    df_stocks = rbind(df_stocks, dfr)
+  #   precio = urls_stocks$u[i] %>%
+  #     read_html() %>% 
+  #     html_nodes(xpath = '//*[@class="stock-quote__price-section__mdc"]') %>%
+  #     html_text()
+  #   precio = as.numeric(gsub(",", ".", precio))
+  #   dfr = data.frame(Stock = urls_stocks$Stock[i], precio = precio, fecha = today)
+  #   df_stocks = rbind(df_stocks, dfr)
 
-  }
+  # }
 
-  newdf_stocks = rbind(olddf_stocks, df_stocks)
-  newdf_stocks = unique(subset(newdf_stocks, !is.na(precio)))
-  write.csv(newdf_stocks, "price_stocks.csv", row.names = FALSE)
+  # newdf_stocks = rbind(olddf_stocks, df_stocks)
+  # newdf_stocks = unique(subset(newdf_stocks, !is.na(precio)))
+  # write.csv(newdf_stocks, "price_stocks.csv", row.names = FALSE)
 
 } else { print(paste("[STOCKS] Already have data from today, NOT updating:", today)) }
